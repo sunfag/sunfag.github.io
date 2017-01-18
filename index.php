989e4568dd4f53911f07a71b73f392a9fcf397ac
heroverin <admin@heroverin.ru>
2017-01-18 23:36:09 +0200

    /

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<title>Sunfag</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<meta name="theme-color" content="#555555">
+<meta property="og:image" content="/images/og.jpg">
+<link rel="shortcut icon" href="/images/favicon.ico">
+
+<link rel="stylesheet" href="/css/reset.css" media="all">
+<link rel="stylesheet" href="/css/ut-fontface.css" media="all">
+<link rel="stylesheet" href="/css/font-awesome.css" media="all">
+<link rel="stylesheet" href="/css/flexslider.css" media="all">
+<link rel="stylesheet" href="/css/magnific-popup.css" media="all">
+<link rel="stylesheet" href="/css/ut-responsive-grid.css" media="all">
+<link rel="stylesheet" href="/css/color.css" media="all">
+<link rel="stylesheet" href="/css/style.css" media="all">
+
+<script src="/js/jquery.min.js"></script>
+<script src="/js/SmoothScroll.js"></script>
+<script src="/js/modernizr.js"></script>
+<script src="/js/device.min.js"></script>
+
+</head>
+<body id="mainsite">
+
+<section class="hero ha-waypoint parallax-section parallax-background" data-animate-up="ha-header-hide" data-animate-down="ha-header-hide">	
+<a id="bgndVideo" class="player" data-property="{videoURL:'46Q10xkx-kc',containment:'body',autoPlay:true, mute:false, startAt:0, opacity:1}"></a> 
+<div class="parallax-overlay parallax-overlay-1"></div> 
+<div class="grid-container">   		
+<div class="hero-holder grid-70 mobile-grid-100 tablet-grid-100">
+<div class="hero-inner">
+<div class="hero-title-holder"><h1 class="hero-title">Sunfag</h1></div>
+<span class="btn-holder">
+<a href="https://vk.com/sunfag" target="_blank" class="hero-btn">VK</a>
+<a href="https://vk.com/heroverin" target="_blank" class="hero-btn">group</a>
+</span>
+</div>
+</div> 													
+</div>
+</section>
+
+<script src="/js/jquery.mb.YTPlayer.js"></script>
+<script src="/js/jquery.flexslider-min.js"></script>
+<script src="/js/jquery.parallax.min.js"></script>
+<script src="/js/jquery.scrollTo.min.js"></script>
+<script src="/js/waypoints.min.js"></script>
+<script src="/js/jquery.magnific-popup.min.js"></script>
+<script src="/js/jquery.custom.js"></script>
+    
+</body>
+</html>
